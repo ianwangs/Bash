700eb6bfd4cdeaf8355eb2f1e3abc440bbfe687b
Ian Wang <ianwangs@gmail.com>
2016-09-16 16:13:31 -0700

    Add files via upload

---
+++ Hello_World.sh
@@ -0,0 +1,10 @@
+# Before you run this scrip under Linux prompt, make sure you'd changed the script's file permissions to allow it to be execute.
+# For example, "chmod +X Hello_World.sh"
+# Execute the file under Linux prompt, "./Hello_World.sh"
+
+#!/bin/bash 		#=> this is a shebang line,. The interpreter of BASH is located at /bin/bash
+echo "\"Hello World!\""
+
+# To print the quotation mark, " from the line of Hello_World. ", add an escape character, 
+# which is the backslash (\). So this special character will tell the interpreter to 
+# ignore the special properties of the next character and just treat it as part of a given string. 
